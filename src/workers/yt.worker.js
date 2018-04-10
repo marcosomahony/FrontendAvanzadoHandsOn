ff18791408e47e009d036540d4dea1cb39b8fc7f
CBSanta <catalin.beniamin@babel.es>
2018-04-10 12:53:59 +0200

    api videos, todo css

---
+++ src/workers/yt.worker.js
@@ -0,0 +1,25 @@
+import { ytapikey } from './api_key.js'
+
+(function () {
+    const ytquery = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=pinguinos&key=" + ytapikey;
+    const res = JSON.parse(httpGet(ytquery));
+    res.items.forEach(
+        element => postMessage(process(element))
+    );
+})()
+
+function httpGet(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+function process(elem) {
+    return {
+        title: elem.snippet.title,
+        channel: elem.snippet.channelTitle,
+        description: elem.snippet.description,
+        img: elem.snippet.thumbnails.medium.url
+    }
+}
